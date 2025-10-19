6d74f83f0d501f4fee776779f68be0777dd109ed
Mohamed Aboellil <mohamed.aboellil0@gmail.com>
2025-10-19 09:37:42 +0300

    Set default role to admin for superusers in UserManager

--- ecommerce_project/api/models.py
+++ ecommerce_project/api/models.py
@@ -17,6 +17,7 @@ class UserManager(BaseUserManager):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
         extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('role', 'admin')  # Set role to admin for superusers
         if extra_fields.get('is_staff') is not True:
             raise ValueError('Superuser must have is_staff=True.')
         if extra_fields.get('is_superuser') is not True:
